e42f301f543302013456f4d4d585c9c5b5c70179
Prince Tomar <prince@farmsetu.co>
2025-02-03 12:36:54 +0530

    Updated Authenticated screen in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
         create: (context) => AuthenticationCubit(
           repository: sl(),
         ),
-        child: MovieScreen(),
+        child: AuthenticationScreen(),
       ),
     );
   }
